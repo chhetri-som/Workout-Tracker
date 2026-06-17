92bf886411c5b400fe4d76dc69a25d23f09f741e
Anish.Chhetri <chhetri.anish14@gmail.com>
2026-06-18 00:42:22 +0530

    initialized the routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 const app = express()
 
-// basic middleware to track requests in console
+// middleware to check if request has a body (POST && PATCH)
+app.use(express.json())
+// middleware to track requests in console
 app.use((req, res, next) => {
     console.log(req.method, req.path)
     next()
 })
 
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to Workout Tracker.'})
-})
+// routes
+app.use('/workouts', workoutRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`express server running in port ${process.env.PORT}...`)
